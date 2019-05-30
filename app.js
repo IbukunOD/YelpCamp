81068a1469bf2f97cc0b62ae878f58999e4d5f6c
Oduniyi Ibukun <oduniyi.i@husky.neu.edu>
2019-05-30 19:20:18 -0400

    added auth to the application

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ var express 	= require("express")
 	mongoose 	= require("mongoose"),
 	Campground 	= require("./models/campground"),
 	Comment 	= require("./models/comment"),
-	//User 		= require("./models/user");
-	seedDB      = require("./seed")
+	User 		= require("./models/user"),
+	passport 	= require("passport"),
+	localStrategy	= require("passport-local"),
+	seedDB      = require("./seed");
 
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public"))
 
 /* ar campground = [{name: "spain", image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"}, 
 {name: "spain", image: "https://images.unsplash.com/photo-1484960055659-a39d25adcb3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"}, 
@@ -23,7 +26,21 @@ app.set("view engine", "ejs");
 {name: "spain", image: "https://images.unsplash.com/photo-1528892677828-8862216f3665?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"}]
  */
  
+
 seedDB();
+
+//PASSPORT CONFIG
+app.use(require("express-session")({
+	secret: "Once again Rusty wins cutest dog",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (request, response) => {
 	response.render("landing");
@@ -111,6 +128,30 @@ app.post("/campground/:id/comments",(request, response) => {
 })
 
 })
+
+//AUTH Routes
+//=============
+
+app.get("/register",(request,response)=>{
+response.render("register/register");
+});
+
+app.post("/register",(request,response)=>{
+	
+	User.register(new User({username: request.body.register.username}), request.body.register.password,(error, user)=>{
+		if(error){
+			console.log(error);
+			return response.render("register/register");
+		}
+			passport.authenticate("local")(request, response, () => {
+				console.log("we got here!!!!!!!!!!!!");
+				response.redirect("/campground");
+			})
+		
+	})
+})
+
+
 
 
 
